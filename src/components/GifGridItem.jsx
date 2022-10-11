712b0cf9cb26c1530b686464a8661abae4c587a0
juan correa <juancorrea@MacBook-Pro-de-juan.local>
2022-10-11 15:13:41 -0500

    add proptypes to <GifGridItem/> component

--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 
 export const GifGridItem = ({title, url, id}) => {
     return (
@@ -8,4 +10,9 @@ export const GifGridItem = ({title, url, id}) => {
         </div>
         </>
     )
+}
+
+GifGridItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
 }
